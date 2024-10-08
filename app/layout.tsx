ab7c2110c72c8f9bc701b860a4d91259e43e7a8c
YASIN <56336030+Yasin-13@users.noreply.github.com>
2024-10-08 10:07:53 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -24,12 +24,29 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
-      </body>
+   <html lang="en">
+      <head>
+        {/* Matomo Analytics */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+               var _paq = window._paq = window._paq || [];
+  /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
+  _paq.push(['trackPageView']);
+  _paq.push(['enableLinkTracking']);
+  (function() {
+    var u="https://webanalyticstesting2024.matomo.cloud/";
+    _paq.push(['setTrackerUrl', u+'matomo.php']);
+    _paq.push(['setSiteId', '1']);
+    var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+    g.async=true; g.src='https://cdn.matomo.cloud/webanalyticstesting2024.matomo.cloud/matomo.js'; s.parentNode.insertBefore(g,s);
+  })();
+            `,
+          }}
+        />
+        {/* End Matomo Analytics */}
+      </head>
+      <body className={inter.className}>{children}</body>
     </html>
   );
 }
