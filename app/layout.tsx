ca236239a79702437f886c0da865e9a24faafd1e
YASIN <56336030+Yasin-13@users.noreply.github.com>
2024-10-08 10:09:53 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
+import Head from "next/head"; // Import Head from next/head
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -24,29 +25,33 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-   <html lang="en">
-      <head>
-        {/* Matomo Analytics */}
+    <html lang="en">
+      <Head>
+        {/* Matomo Tracking Code */}
         <script
           dangerouslySetInnerHTML={{
             __html: `
-               var _paq = window._paq = window._paq || [];
-  /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
-  _paq.push(['trackPageView']);
-  _paq.push(['enableLinkTracking']);
-  (function() {
-    var u="https://webanalyticstesting2024.matomo.cloud/";
-    _paq.push(['setTrackerUrl', u+'matomo.php']);
-    _paq.push(['setSiteId', '1']);
-    var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
-    g.async=true; g.src='https://cdn.matomo.cloud/webanalyticstesting2024.matomo.cloud/matomo.js'; s.parentNode.insertBefore(g,s);
-  })();
+              var _paq = window._paq = window._paq || [];
+              _paq.push(['trackPageView']);
+              _paq.push(['enableLinkTracking']);
+              (function() {
+                var u="https://webanalyticstesting2024.matomo.cloud/";
+                _paq.push(['setTrackerUrl', u+'matomo.php']);
+                _paq.push(['setSiteId', '1']);
+                var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+                g.async=true; g.src='https://cdn.matomo.cloud/webanalyticstesting2024.matomo.cloud/matomo.js'; 
+                s.parentNode.insertBefore(g,s);
+              })();
             `,
           }}
         />
-        {/* End Matomo Analytics */}
-      </head>
-      <body className={inter.className}>{children}</body>
+        {/* End Matomo Code */}
+      </Head>
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        {children}
+      </body>
     </html>
   );
 }
